9a540ca60ac32c68a2fb127914efb0de8a4cd077
etunaa <104025100+etunaa@users.noreply.github.com>
2022-05-23 20:06:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("ola")
+var city=[
+    ["Tbilisi","Madrid","Rome"],
+    [1,2,3],
+    ["beautiful","big","ancient"],
+]
+
+var RomeInfo= city[0][2];
+var twoInfo=city[1][1];
+var ancientInfo=city[2][2];
+
+console.log(Romeinfo +" " + twoInfo +" " + ancientInfo);
+
